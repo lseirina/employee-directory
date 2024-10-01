197827a55955956b04207ae7a4dcce4ccfb014fd
root <lseirina87@gmail.com>
2024-10-01 21:52:05 +0300

    Added F to CharField

--- app/core/models.py
+++ app/core/models.py
@@ -4,7 +4,7 @@ from django.db import models
 
 class Employee(models.Model):
     full_name = models.Charfield(max_length=255)
-    position = models.Charfield(max_length=255)
+    position = models.CharField(max_length=255)
     hire_date = models.DateField()
     salary = models.DecimalField(max_digits=10, decimal_places=2)
     manager = models.ForeignKey('self', null=True, blank=True,
