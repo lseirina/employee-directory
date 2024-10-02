6cf984233a22d8e6277d7b33f81c408f64ba6fae
root <lseirina87@gmail.com>
2024-10-02 12:19:58 +0300

    Added test  model Employee

--- app/core/tests/test_models.py
+++ app/core/tests/test_models.py
@@ -1,8 +1,18 @@
 """Tests for models."""
 
 from django.test import TestCase
-from core.models import Employee
+from core import models
+from datetime import date
 
 
-class ModelTests(TestCase):
-    def test_
+class ModelTest(TestCase):
+    def test_create_employee(self):
+        """Test creating employee."""
+        employee = models.Employee.objects.create(
+            full_name='Robert Downy-Jn',
+            position='Chef-manager',
+            hire_date=date(2024, 2, 12),
+            salary=158000.00,
+        )
+
+        self.assertEqual(employee.full_name, str(employee))
